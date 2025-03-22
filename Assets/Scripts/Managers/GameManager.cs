0466c1f2cc5068ed55823e0359a84da605d98645
AmitDeDev <amitde2@gmail.com>
2025-03-22 13:31:32 +0200

    clean up and FPS changes

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -81,6 +81,9 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+        
         if (Instance == null) Instance = this;
         else Destroy(gameObject);
     }
